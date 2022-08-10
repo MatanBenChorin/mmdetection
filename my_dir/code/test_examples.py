27eb635b67e2327868ee4154a061fda5c5edd149
matanb <matan.bh1@gmail.com>
2022-08-10 16:33:41 +0300

    .

---
+++ my_dir/code/test_examples.py
@@ -0,0 +1,6 @@
+"""
+python tools/test.py \
+    configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py \
+    checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth \
+    --show-dir faster_rcnn_r50_fpn_1x_results
+"""
