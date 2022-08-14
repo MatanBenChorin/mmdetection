4933b87cf1fb4d7fe2fc9d3f85a3044c6f7e97e0
matanb <matan.bh1@gmail.com>
2022-08-14 15:30:28 +0300

    slim coco config and test

--- my_dir/code/example_test.py
+++ my_dir/code/example_test.py
@@ -9,14 +9,31 @@ import tools.test as test
 import sys
 import os
 
-def main():
+
+def run_test(config_file,checkpoint_file,imgs_result_dir):
     os.chdir("../../")
-    sys.argv.append("configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py") # CONFIG_FILE
-    sys.argv.append("checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth") # CHECKPOINT_FILE
-    sys.argv.append("--show-dir=my_dir/results/faster_rcnn_r50_fpn_1x_results/imgs")
-    sys.argv.append("--show-dir=my_dir/results/faster_rcnn_r50_fpn_1x_results/imgs")
+    sys.argv.append(config_file)  # CONFIG_FILE
+    sys.argv.append(checkpoint_file)  # CHECKPOINT_FILE
+    sys.argv.append("--show-dir="+imgs_result_dir)
     sys.argv.append("--show")
     test.main()
+
+def main():
+    # genuine_coco()
+    slim_coco()
+
+def genuine_coco():
+    config_file = 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+    checkpoint_file = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+    imgs_result_dir = 'my_dir/results/faster_rcnn_r50_fpn_1x_results/imgs'
+    run_test(config_file, checkpoint_file, imgs_result_dir)
+
+def slim_coco():
+    config_file = 'configs/my_configs/faster_rcnn_r50_fpn_1x_coco_slim.py'
+    checkpoint_file = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+    imgs_result_dir = 'my_dir/results/faster_rcnn_r50_fpn_1x_results/imgs'
+    run_test(config_file, checkpoint_file, imgs_result_dir)
+
 
 
 if __name__ == '__main__':
